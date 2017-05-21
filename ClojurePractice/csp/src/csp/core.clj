6f8d3cadb4673e7a0655f6f22a7710d4c93c6639
zrma <bulbitain@gmail.com>
2017-05-21 21:45:33 +0900

    클로저 구구단

--- ClojurePractice/csp/src/csp/core.clj
+++ ClojurePractice/csp/src/csp/core.clj
@@ -5,4 +5,12 @@
 	[x]
 	(println x "Hello, World!"))
 
+(defn gugu
+	[]
+	(doall
+		(for [x (range 2 10) y (range 1 10)]
+			(println (format "%d x %d = %d" x y (* x y))))))
+
+
 (foo "Tester")
+(gugu)
