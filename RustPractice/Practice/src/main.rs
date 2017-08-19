fcfea90df058d7313b4beabf996fcf82d858395b
zrma <bulbitain@gmail.com>
2017-08-20 08:10:31 +0900

    go언어 src 폴더 github.com 저장소 gitignore 추가

--- RustPractice/Practice/src/main.rs
+++ RustPractice/Practice/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
+    let mut vec = Vec::new();
+    vec.push(1);
+    vec.push(2);
+
+    assert_eq!(vec.len(), 2);
+    assert_eq!(vec[0], 1);
+
     println!("Hello, world!");
+
+    for x in &vec {
+        println!( "{}", x );
+    }
 }
