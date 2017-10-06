478e453360bcf3596981798cb7546319d7ea5cbb
zrma <bulbitain@gmail.com>
2017-10-06 21:30:55 +0900

    자잘

--- RustPractice/Practice/src/main.rs
+++ RustPractice/Practice/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let mut vec = Vec::new();
-    vec.push(1);
-    vec.push(2);
+    &vec.push(1);
+    &vec.push(2);
 
     assert_eq!(vec.len(), 2);
     assert_eq!(vec[0], 1);
@@ -9,6 +9,6 @@ fn main() {
     println!("Hello, world!");
 
     for x in &vec {
-        println!( "{}", x );
+        println!("{}", x);
     }
 }
