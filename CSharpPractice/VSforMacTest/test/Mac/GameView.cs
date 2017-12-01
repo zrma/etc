2d37c291477a76b69f046a4929cb2bf13c37e246
zrma <bulbitain@gmail.com>
2017-12-01 21:46:33 +0900

    자잘

---
+++ CSharpPractice/VSforMacTest/test/Mac/GameView.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using AppKit;
+using SceneKit;
+using Foundation;
+using CoreGraphics;
+
+namespace test.Mac
+{
+    [Register("GameView")]
+    public class GameView : SCNView
+    {
+        public GameView(IntPtr Handle) : base(Handle)
+        {
+        }
+
+        // forward click event to the game view controller
+        public override void MouseDown(NSEvent theEvent)
+        {
+            // Called when a mouse click occurs
+
+            // check what nodes are clicked
+            var p = ConvertPointFromView(theEvent.LocationInWindow, null);
+            var hitResults = HitTest(p, (NSDictionary)null);
+
+            // check that we clicked on at least one object
+            if (hitResults.Length > 0)
+            {
+                // retrieved the first clicked object
+                var result = hitResults[0];
+
+                // get its material
+                var material = result.Node.Geometry.FirstMaterial;
+
+                // highlight it
+                SCNTransaction.Begin();
+                SCNTransaction.AnimationDuration = 0.5f;
+
+                // on completion - unhighlight
+                SCNTransaction.SetCompletionBlock(() =>
+                {
+                    SCNTransaction.Begin();
+                    SCNTransaction.AnimationDuration = 0.5f;
+
+                    material.Emission.Contents = NSColor.Black;
+
+                    SCNTransaction.Commit();
+                });
+
+                material.Emission.Contents = NSColor.Red;
+
+                SCNTransaction.Commit();
+            }
+
+            base.MouseDown(theEvent);
+        }
+    }
+}
+
