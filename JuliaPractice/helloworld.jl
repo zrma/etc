548e469899df7aae926a95fa8ed0c8f02211082e
zrma <bulbitain@gmail.com>
2018-05-21 00:16:10 +0900

    Nim 을 세팅해 보았다.

--- JuliaPractice/helloworld.jl
+++ JuliaPractice/helloworld.jl
@@ -1,10 +1,17 @@
-total = 0
+function test( n::Int64 )
+    total = 0
 
-for i in 1:10000000
-    if i % 2 == 0
-        total += 1
+    for i in 1:n
+        if i % 2 == 0
+            total += 1
+        end
     end
+
+    return total
 end
 
-println( "total : ", total )
+@time result = test( 10 )
+println( result )
 
+@time result = test( 1000000000 )
+println( result )
