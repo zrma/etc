825686340713c4ecce7ea7097f10a60cb5552012
zrma <bulbitain@gmail.com>
2018-05-29 00:46:38 +0900

    rust 에서 dll 생성 및 동적 로딩 연습 - 메모리 관련해서 seg fault 발생 중...

---
+++ RustPractice/embed/src/lib.rs
@@ -0,0 +1,29 @@
+use std::thread;
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+
+#[no_mangle]
+pub extern fn process() {
+    let handles: Vec<_> = (0..10).map(|_| {
+        thread::spawn(|| {
+            let mut x = 0;
+            for _ in 0..5_000_000 {
+                x += 1
+            }
+            x
+        })
+    }).collect();
+
+    for h in handles {
+        println!("Thread finished with count={}",
+                 h.join().map_err(|_| "Could not join a thread!").unwrap());
+    }
+}
