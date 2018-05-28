72e9f0252efd0765c63611d62bafe8742ccd5be9
zrma <bulbitain@gmail.com>
2018-05-28 23:42:21 +0900

    Nim을 곰곰이 관찰

--- NimPractice/test/hello.nim
+++ NimPractice/test/hello.nim
@@ -1,1 +1,14 @@
-echo "Hello, World"
+import strformat
+
+type
+    Person = object
+        name*: string
+        age: Natural
+
+var people = [
+    Person(name: "Tester01", age:20),
+    Person(name: "Abdula", age:15)
+]
+
+for person in people:
+    echo fmt"{person.name} is {person.age} years old"
