9448ca4f1d15f9c10f55134dc568fddca1703234
zrma <bulbitain@gmail.com>
2018-05-30 03:58:46 +0000

    동적 라이브러리 로딩 할 때 멀티 OS 지원하도록 설정

--- RustPractice/Practice/src/main.rs
+++ RustPractice/Practice/src/main.rs
@@ -4,8 +4,17 @@ extern crate dlopen;
 
 use dlopen::symbor::Library;
 
+#[cfg(target_os = "windows")]
+const FILE_NAME: &str= "./embed.dll";
+
+#[cfg(target_os = "ubuntu")]
+const FILE_NAME : &str= "./libembed.so";
+
+#[cfg(target_os = "macos")]
+const FILE_NAME : &str= "./libembed.dylib";
+
 fn main() {
-    let lib = Library::open("./libembed.dylib").expect("Failed to load library");
+    let lib = Library::open(FILE_NAME).expect("Failed to load library");
 
     let func = unsafe { lib.symbol_cstr::<fn()>(const_cstr!("process").as_cstr()) }.unwrap();
 
