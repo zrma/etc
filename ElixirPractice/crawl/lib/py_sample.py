ba9e209fb95d29f59b69fab848dcf7a09395e604
zrma <bulbitain@gmail.com>
2018-07-02 02:30:43 +0900

    elixir + keras predict_sin 샘플 추가

--- ElixirPractice/crawl/lib/py_sample.py
+++ ElixirPractice/crawl/lib/py_sample.py
@@ -1,3 +1,18 @@
-def value_receive( value ):
-  print( "I'm Python...receive {0}".format( value ) )
-  return value * 10
+def value_receive(value):
+    print("I'm Python...receive {0}".format(value))
+    return value * 10
+
+
+def list_receive(list):
+    print("I'm Python...receive {0}".format(list))
+    return sum(list)
+
+
+class Sample(object):
+    def __init__(self):
+        print("  on python: __init__()")
+        self.message = "Hello "
+
+    def arrange(self, name):
+        print("  on python: arrange()")
+        return self.message + name.decode("utf-8")
