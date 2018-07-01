d8f9c95bd3e01ab22b35e16236ae37a9728dcb56
zrma <bulbitain@gmail.com>
2018-07-02 02:30:43 +0900

    elixir + keras predict_sin 샘플 추가

--- ElixirPractice/crawl/lib/pyex.ex
+++ ElixirPractice/crawl/lib/pyex.ex
@@ -1,8 +1,29 @@
 defmodule Pyex do
-  def value_receive( value ) do
-    { :ok, py_exec } = :python.start( [ python_path: 'lib' ] )
-    result = :python.call( py_exec, :py_sample, :value_receive, [ value ] )
+  def value_receive(value) do
+    {:ok, py_exec} = :python.start([python_path: 'lib'])
+    result = :python.call(py_exec, :py_sample, :value_receive, [value])
     IO.puts "received from python: #{result}"
-    :python.stop( py_exec )
+    :python.stop(py_exec)
+  end
+
+  def list_receive(list) when is_list(list) do
+    {:ok, py_exec} = :python.start([python_path: 'lib'])
+    result = :python.call(py_exec, :py_sample, :list_receive, [list])
+    IO.puts "received from python: #{result}"
+    :python.stop(py_exec)
+  end
+
+  def call_class(value) do
+    {:ok, py_exec} = :python.start([python_path: 'lib'])
+    object = :python.call(py_exec, :py_sample, :Sample, [])
+    result = :python.call(py_exec, :py_sample, :"Sample.arrange", [object, value])
+    IO.puts "received from python: #{result}"
+    :python.stop(py_exec)
+  end
+
+  def predict() do
+    {:ok, py_exec} = :python.start([python_path: 'lib'])
+    :python.call(py_exec, :predict_sin, :predict, [])
+    :python.stop(py_exec)
   end
 end
