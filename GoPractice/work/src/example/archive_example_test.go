7ddf8b541f0cd0959d5069ed7127118206345525
zrma <bulbitain@gmail.com>
2018-07-10 02:09:19 +0900

    tar 헤더의 인자(사이즈) 잘못 넣은 부분 수정, archive 테스트 코드 추가

--- GoPractice/work/src/example/archive_example_test.go
+++ GoPractice/work/src/example/archive_example_test.go
@@ -1,1 +1,15 @@
 package main
+
+func ExampleArchive() {
+	archive()
+	// Output:
+	// Contents of readme.txt :
+	// This archive contains some text files.
+	// Contents of gopher.txt :
+	// Gopher name:
+	// George
+	// Geoffrey
+	// Gonzo
+	// Contents of note.txt :
+	// Get animal handling license.
+}
