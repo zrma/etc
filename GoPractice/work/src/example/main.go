2cfc4674ee9b97cc72ac92bc550639076c7505d9
zrma <bulbitain@gmail.com>
2018-07-10 02:08:09 +0900

    sentry 오류 보고 안 되는 문제 수정( url 끝 부분의 // -> / ), raven.CapturePanic() -> raven.CapturePanicAndWait()

--- GoPractice/work/src/example/main.go
+++ GoPractice/work/src/example/main.go
@@ -8,12 +8,12 @@ import (
 
 func main() {
 
-	if err := raven.SetDSN("https://e52a68c3b0314a45a9cd871bc6abe05c:0e4acc31f2d143b0ab4f2bad77cc2279@sentry.team504.co.kr//3"); err != nil {
+	if err := raven.SetDSN("https://e52a68c3b0314a45a9cd871bc6abe05c:0e4acc31f2d143b0ab4f2bad77cc2279@sentry.team504.co.kr/3"); err != nil {
 		log.Panic(err)
 	}
 	defer raven.Close()
 
-	raven.CapturePanic(func() {
+	raven.CapturePanicAndWait(func() {
 		archive()
 	}, nil)
 }
