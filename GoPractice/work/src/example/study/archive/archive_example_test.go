b1aa4bef1d16701b8c9a00850a13ea12a755f11f
zrma <bulbitain@gmail.com>
2018-07-13 01:12:35 +0900

    패키지 분리

---
+++ GoPractice/work/src/example/study/archive/archive_example_test.go
@@ -0,0 +1,30 @@
+package archive
+
+func ExampleTarPhase() {
+	TarPhase()
+	// Output:
+	// Contents of readme.txt :
+	// This phase contains some text files.
+	// Contents of gopher.txt :
+	// Gopher name:
+	// George
+	// Geoffrey
+	// Gonzo
+	// Contents of note.txt :
+	// Get animal handling license.
+
+}
+
+func ExampleZipPhase() {
+	ZipPhase()
+	// Output:
+	// Contents of readme.txt :
+	// This phase contains some text files.
+	// Contents of gopher.txt :
+	// Gopher name:
+	// George
+	// Geoffrey
+	// Gonzo
+	// Contents of note.txt :
+	// Get animal handling license.
+}
