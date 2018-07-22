1ee518f48515d46cd9003dbadeca9e3f80348957
zrma <bulbitain@gmail.com>
2018-07-22 14:27:38 +0900

    유틸성 함수의 파일 출력 및 제거 함수 유닛테스트 추가

---
+++ GoPractice/work/src/example/util/file_test.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"bytes"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWriteFile(t *testing.T) {
+	reader := strings.NewReader("Hello World!")
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(reader)
+
+	fileName := "/tmp/test.txt"
+
+	WriteFile(buf, fileName)
+	_, err := os.Stat(fileName)
+	assert.True(t, err == nil || os.IsExist(err), "file not created!", fileName)
+
+	RemoveFile(fileName)
+	_, err = os.Stat(fileName)
+	assert.True(t, os.IsNotExist(err), "file not created!", fileName)
+}
