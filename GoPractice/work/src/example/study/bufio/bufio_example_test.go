dec37a17595714fca36742ae2edc2a52dfd1d67f
zrma <bulbitain@gmail.com>
2018-07-22 23:15:16 +0900

    gometalinter 권고사항 수정

--- GoPractice/work/src/example/study/bufio/bufio_example_test.go
+++ GoPractice/work/src/example/study/bufio/bufio_example_test.go
@@ -1,10 +1,33 @@
 package bufio
 
-func ExampleScan() {
-	Scan()
+import (
+	"bytes"
+	"io"
+	"strings"
+)
+
+func ExampleCustom() {
+	Custom()
 	// Output:
 	// 1234
 	// 5678
 	// strconv.ParseInt: parsing "1234567901234567890": value out of range
 	// strconv.ParseInt: parsing "1234567901234567890": value out of range
+}
+
+func ExampleEmptyFinalToken() {
+	EmptyFinalToken()
+	// Output:
+	// "1" "2" "3" "4" ""
+}
+
+func ExampleLines() {
+	r := strings.NewReader("Hello World!")
+	buf := new(bytes.Buffer)
+	reader := io.TeeReader(r, buf)
+
+	Lines(&reader)
+	Lines(nil)
+	// Output:
+	// Hello World!
 }
