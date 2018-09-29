bafea07afed2d241048a94a8026747c7e9a6ac33
zrma <bulbitain@gmail.com>
2018-09-30 01:56:10 +0900

    spark practice - Intellij + SBT

---
+++ SparkPractice/spark/src/main/scala/com/example/chapter4/SundayCount.scala
@@ -0,0 +1,36 @@
+package com.example.chapter4
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+import org.joda.time.{DateTime, DateTimeConstants}
+import org.joda.time.format.DateTimeFormat
+
+object SundayCount {
+  def main(args: Array[String]): Unit = {
+    if (args.length < 1) {
+      throw new IllegalArgumentException("parameter needs - file path")
+    }
+
+    val filePath = args(0)
+    val conf = new SparkConf().setAppName("SundayCount").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    try {
+      val textRDD = sc.textFile(filePath)
+
+      val dateTimeRDD = textRDD.map(dateStr => {
+        val pattern = DateTimeFormat.forPattern("yyyyMMdd")
+        DateTime.parse(dateStr, pattern)
+      })
+
+      val sundayRDD = dateTimeRDD.filter(
+        dateTime => dateTime.getDayOfWeek == DateTimeConstants.SUNDAY
+      )
+
+      val numOfSunday = sundayRDD.count
+      println(s"$numOfSunday found")
+    } finally {
+      sc.stop()
+    }
+  }
+}
