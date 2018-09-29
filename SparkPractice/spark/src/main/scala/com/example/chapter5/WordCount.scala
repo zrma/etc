d0f3a97c2dfbb68b4ee85a7cef75028952f02375
zrma <bulbitain@gmail.com>
2018-09-30 01:56:10 +0900

    spark practice - Intellij + SBT

---
+++ SparkPractice/spark/src/main/scala/com/example/chapter5/WordCount.scala
@@ -0,0 +1,30 @@
+package com.example.chapter5
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    require(args.length >= 1, "parameter needs - file path")
+
+    val conf = new SparkConf().setAppName("WordCount").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    try {
+      val filePath = args(0)
+      val wordCntRDD =
+        sc.textFile(filePath)
+          .flatMap(_.split("[ ,.]"))
+          .filter(_.matches("""\p{Alnum}+"""))
+          .map((_, 1))
+          .reduceByKey(_ + _)
+          .sortByKey(ascending = true)
+          .map({ case (count, word) => (word, count) })
+          .sortByKey(ascending = false)
+          .take(3)
+
+      wordCntRDD.foreach(println)
+    } finally {
+      sc.stop()
+    }
+  }
+}
