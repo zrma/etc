201d7368a2e25a74181a313a6f15a13b1537774f
zrma <bulbitain@gmail.com>
2018-09-30 16:28:24 +0900

    모던 C++ 함수형 프로그래밍 연습 1장

---
+++ CppPractice/ModernCppFunctional/ModernCppFunctional/decltype.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+
+#include "decltype.h"
+
+const int Func1();
+const int& Func2();
+
+struct X { double d;  };
+
+void DeclTypePractice()
+{
+	std::cout << "result of add : " << Add<int, int>(3, 5) << std::endl;
+
+	const auto i = 0;
+	const X* x = new X();
+
+	decltype(Func1) F1;
+	decltype(Func2) F2;
+	// decl type(i) i1 = i;
+	const auto i1 = i;
+	
+	const decltype(x->d) d1 = 0;
+	// decl type((x->d)) d2 = d1;
+	const auto& d2 = d1;
+
+	std::cout << typeid(F1).name() << std::endl;
+	std::cout << typeid(F2).name() << std::endl;
+	_CRT_UNUSED(i1);
+	_CRT_UNUSED(d2);
+
+	delete x;
+}
+
+const int Func1()
+{
+	return 0;
+}
+
+const int & Func2()
+{
+	// TODO: 여기에 반환 구문을 삽입합니다.
+	return std::forward<int&&>(0);
+}
---
+++ CppPractice/ModernCppFunctional/ModernCppFunctional/decltype.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+template<typename I, typename J>
+auto Add(I i, J j) -> decltype(i + j)
+{
+	return i + j;
+}
+
+void DeclTypePractice();
