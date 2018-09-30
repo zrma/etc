e7a59a18519caae8c75a92f28190eaa7bf434e99
zrma <bulbitain@gmail.com>
2018-09-30 16:28:24 +0900

    모던 C++ 함수형 프로그래밍 연습 1장

---
+++ CppPractice/ModernCppFunctional/ModernCppFunctional/auto.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+inline decltype(auto) AutoPractice()
+{
+	std::cout << "[auto.cpp]" << std::endl;
+
+	// 여러 개의 auto 타입 변수 생성
+	constexpr auto a = 1;
+	constexpr auto b = 1.0;
+	constexpr auto c = a + b;
+	const auto d = { b, c };
+	for (auto&& it : d) { std::cout << it << std::endl; }
+
+	// 변수의 실제 타입 출력
+	std::cout << "type of a : " << typeid(a).name() << std::endl;
+	std::cout << "type of b : " << typeid(b).name() << std::endl;
+	std::cout << "type of c : " << typeid(c).name() << std::endl;
+	std::cout << "type of d : " << typeid(d).name() << std::endl;
+
+	return c;
+}
