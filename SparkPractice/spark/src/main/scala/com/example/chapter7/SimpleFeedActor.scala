067771b9e4b8532460c282349284ab50931164c7
zrma <bulbitain@gmail.com>
2018-10-01 01:02:08 +0900

    spark-streaming akka receiver 작업 중이지만 잘 안 됨...

--- SparkPractice/spark/src/main/scala/com/example/chapter7/SimpleFeedActor.scala
+++ SparkPractice/spark/src/main/scala/com/example/chapter7/SimpleFeedActor.scala
@@ -14,7 +14,9 @@ case class SubscribeReceiver(receiverActor: ActorRef)
 
 case class UnsubscribeReceiver(receiverActor: ActorRef)
 
-class SimpleFeederActor(inputFile: String, interval: Int) extends Actor with Logging {
+class SimpleFeederActor(inputFile: String, interval: Int)
+    extends Actor
+    with Logging {
 
   var receivers = List.empty[ActorRef]
 
@@ -54,40 +56,39 @@ class SimpleFeederActor(inputFile: String, interval: Int) extends Actor with Log
 object SimpleFeedActor {
 
   def main(args: Array[String]) {
-    val parser = new scopt.OptionParser[SimpleFeedActorConfig]("SimpleFeedActor") {
-      arg[String]("hostname") required() action {
-        (x, c) => c.copy(hostname = x)
-      } text "The hostname to accept connections from remote hosts"
+    val parser =
+      new scopt.OptionParser[SimpleFeedActorConfig]("SimpleFeedActor") {
+        arg[String]("hostname") required () action { (x, c) =>
+          c.copy(hostname = x)
+        } text "The hostname to accept connections from remote hosts"
 
-      arg[Int]("port") required() action {
-        (x, c) => c.copy(port = x)
-      } text "The port number to accept connections from remote hosts"
+        arg[Int]("port") required () action { (x, c) => c.copy(port = x)
+        } text "The port number to accept connections from remote hosts"
 
-      arg[Int]("interval") required() action {
-        (x, c) => c.copy(interval = x)
-      } text "The interval to send messages to remote hosts [msec]"
+        arg[Int]("interval") required () action { (x, c) => c.copy(interval = x)
+        } text "The interval to send messages to remote hosts [msec]"
 
-      arg[String]("inputFile") required() action {
-        (x, c) => c.copy(inputFile = x)
-      } text "The path of the input file"
+        arg[String]("inputFile") required () action { (x, c) =>
+          c.copy(inputFile = x)
+        } text "The path of the input file"
 
-    }
+      }
 
     parser.parse(args, SimpleFeedActorConfig()) exists { config =>
-      val akkaConf = ConfigFactory.parseString(
-        s"""
+      val akkaConf = ConfigFactory.parseString(s"""
            |akka.actor.provider = "akka.remote.RemoteActorRefProvider"
            |akka.remote.netty.tcp.transport-class = "akka.remote.transport.netty.NettyTransport"
            |akka.remote.netty.tcp.hostname = "${config.hostname}"
            |akka.remote.netty.tcp.port = ${config.port}
            |akka.remote.netty.tcp.tcp-nodelay = on
-     """.stripMargin
-      )
+     """.stripMargin)
       val actorSystem = ActorSystem.create("test", akkaConf)
-      val feeder = actorSystem.actorOf(Props(new SimpleFeederActor(config.inputFile, config.interval)), "Feeder")
+      val feeder = actorSystem.actorOf(
+        Props(new SimpleFeederActor(config.inputFile, config.interval)),
+        "Feeder"
+      )
 
       println("Feeder started as:" + feeder)
-
       actorSystem.terminate()
 
       true
