e4ec3ea34fe60fcdff9aeb7cb58417a8dd8a56a7
zrma <bulbitain@gmail.com>
2018-09-30 22:08:09 +0900

    spark-streaming 예제 추가

---
+++ SparkPractice/spark/src/main/scala/com/example/chapter7/SparkStreaming.scala
@@ -0,0 +1,24 @@
+package com.example.chapter7
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.storage.StorageLevel
+
+object SparkStreaming {
+  def main(args: Array[String]): Unit = {
+    Logger.getRootLogger.setLevel(Level.WARN)
+
+    val ss = SparkSession.builder().master("local[*]").appName("SparkStreaming").getOrCreate()
+    val ssc = new StreamingContext(ss.sparkContext, Seconds(10))
+
+    val lines = ssc.socketTextStream("localhost", 9999, StorageLevel.MEMORY_AND_DISK_SER)
+    val words = lines.flatMap(_.split(" ")).filter(_.nonEmpty)
+    val pairs = words.map((_, 1))
+    val wordCounts = pairs.reduceByKey(_ + _)
+
+    wordCounts.print()
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
