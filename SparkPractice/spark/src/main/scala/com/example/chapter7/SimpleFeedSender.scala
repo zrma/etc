32dcc4458f4426bfc163d99ff2e618c4b9e8e0f2
zrma <bulbitain@gmail.com>
2018-10-02 02:00:22 +0900

    akka 제거하고 소켓으로 데이터 전송

---
+++ SparkPractice/spark/src/main/scala/com/example/chapter7/SimpleFeedSender.scala
@@ -0,0 +1,76 @@
+package com.example.chapter7
+
+import java.io.PrintStream
+import java.net.ServerSocket
+
+import scala.io.Source
+
+case class SimpleFeedActorConfig(hostname: String = "localhost",
+                                 port: Int = 12345,
+                                 interval: Int = 500,
+                                 inputFile: String = "input")
+
+object SimpleFeedSender {
+
+  def main(args: Array[String]) {
+    val parser =
+      new scopt.OptionParser[SimpleFeedActorConfig]("SimpleFeedActor") {
+        arg[String]("hostname") required () action { (x, c) =>
+          c.copy(hostname = x)
+        } text "The hostname to accept connections from remote hosts"
+
+        arg[Int]("port") required () action { (x, c) => c.copy(port = x)
+        } text "The port number to accept connections from remote hosts"
+
+        arg[Int]("interval") required () action { (x, c) => c.copy(interval = x)
+        } text "The interval to send messages to remote hosts [msec]"
+
+        arg[String]("inputFile") required () action { (x, c) =>
+          c.copy(inputFile = x)
+        } text "The path of the input file"
+
+      }
+
+    parser.parse(args, SimpleFeedActorConfig()) exists { config =>
+      println("Start server")
+
+      val server = new ServerSocket(config.port)
+      while (true) {
+        val s = server.accept()
+        new Thread() {
+          val out = new PrintStream(s.getOutputStream)
+          override def run() {
+
+            println("Start processing")
+
+            lazy val dataSource = Source.fromFile(config.inputFile)
+
+            try {
+              for (line <- dataSource.getLines) {
+                Thread.sleep(config.interval)
+
+                if (out.checkError()) {
+                  println("disconnected")
+                  return
+                }
+
+                out.print(line)
+                out.flush()
+              }
+            } finally {
+
+              println("Stop processing")
+
+              dataSource.close()
+              out.close()
+              s.close()
+            }
+          }
+        }.start()
+      }
+
+      true
+    }
+  }
+
+}
--- SparkPractice/spark/src/main/scala/com/example/chapter7/SparkStreaming.scala
+++ SparkPractice/spark/src/main/scala/com/example/chapter7/SparkStreaming.scala
@@ -9,10 +9,15 @@ object SparkStreaming {
   def main(args: Array[String]): Unit = {
     Logger.getRootLogger.setLevel(Level.WARN)
 
-    val ss = SparkSession.builder().master("local[*]").appName("SparkStreaming").getOrCreate()
+    val ss = SparkSession
+      .builder()
+      .master("local[*]")
+      .appName("SparkStreaming")
+      .getOrCreate()
     val ssc = new StreamingContext(ss.sparkContext, Seconds(10))
 
-    val lines = ssc.socketTextStream("localhost", 9999, StorageLevel.MEMORY_AND_DISK_SER)
+    val lines =
+      ssc.socketTextStream("localhost", 9999, StorageLevel.MEMORY_AND_DISK_SER)
     val words = lines.flatMap(_.split(" ")).filter(_.nonEmpty)
     val pairs = words.map((_, 1))
     val wordCounts = pairs.reduceByKey(_ + _)
