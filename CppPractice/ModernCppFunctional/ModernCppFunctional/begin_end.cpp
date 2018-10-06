ffd56a01b0767d8b0ff069d161c22344f324d496
zrma <bulbitain@gmail.com>
2018-10-06 09:36:17 +0900

    std::begin, std::end, std::array, std::vector 연습 추가

---
+++ CppPractice/ModernCppFunctional/ModernCppFunctional/begin_end.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+
+#include "begin_end.h"
+
+void BeginEndPractice()
+{
+	std::cout << "[begin_end]" << std::endl;
+
+	// 배열 선언
+	std::array<int, 10> arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+	const auto size = sizeof(arr) / sizeof(arr[0]);
+	static_assert(arr.size() == size);
+
+	for(auto i = 0; i < size; ++i)
+	{
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+
+	// std::array는 직접 복사 및 대입 가능
+	auto clone = arr;
+	arr[5] = 0;
+
+	// Compile Error!
+	// arr[10] = 1;
+
+	for(auto i = std::begin(arr); i != std::end(arr); ++i)
+	{
+		std::cout << *i << " ";
+	}
+	std::cout << std::endl;
+
+	for (const auto& i : clone)
+	{
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+}
--- CppPractice/ModernCppFunctional/ModernCppFunctional/chapter01.cpp
+++ CppPractice/ModernCppFunctional/ModernCppFunctional/chapter01.cpp
@@ -4,9 +4,13 @@
 #include "decltype.h"
 
 #include "chapter01.h"
+#include "begin_end.h"
+#include "vector.h"
 
 void Chapter01()
 {
 	std::cout << AutoPractice() << std::endl;
 	DeclTypePractice();
+	BeginEndPractice();
+	VectorPractice();
 }
