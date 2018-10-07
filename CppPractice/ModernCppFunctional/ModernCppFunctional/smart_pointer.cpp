3f1a37eec85343c57f5989d18b803feb7e7cbd4a
zrma <bulbitain@gmail.com>
2018-10-07 14:57:34 +0900

    std::unique_ptr 연습 추가

---
+++ CppPractice/ModernCppFunctional/ModernCppFunctional/smart_pointer.cpp
@@ -0,0 +1,56 @@
+﻿#include "pch.h"
+
+#include "smart_pointer.h"
+#include "util.h"
+
+struct BodyMass
+{
+	int Id;
+	float Weight;
+
+	BodyMass(const int id, const float weight): Id(id), Weight(weight)
+	{
+		std::cout << "BodyMass is constructed!" << std::endl;
+		std::cout << "Id = " << id << std::endl;
+		std::cout << "Weight = " << weight << std::endl;
+	}
+	BodyMass(const BodyMass&) = default;
+	BodyMass& operator= (const BodyMass&) = default;
+	BodyMass(const BodyMass&& other) noexcept
+	{
+		Id = other.Id;
+		Weight = other.Weight;
+	}
+	BodyMass& operator= (BodyMass&& other) noexcept
+	{
+		if( this != &other)
+		{
+			Id = other.Id;
+			Weight = other.Weight;
+		}
+
+		return *this;
+	}
+
+	~BodyMass()
+	{
+		Id = 0;
+		Weight = 0;
+		std::cout << "BodyMass is destructed!" << std::endl;
+	}
+};
+
+void UniquePtr()
+{
+	PrintTitle("unique_ptr");
+
+	{
+		auto weight = std::make_unique<BodyMass>(1, 165.3f);
+		std::cout << "in scope" << std::endl;
+	}
+}
+
+void SmartPointerPractice()
+{
+	UniquePtr();
+}
