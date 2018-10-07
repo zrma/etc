1368b14a48978948064e2a0b0ce5babfc5ab8a16
zrma <bulbitain@gmail.com>
2018-10-07 22:06:58 +0900

    std::tuple 연습 추가

---
+++ CppPractice/ModernCppFunctional/ModernCppFunctional/tuple.cpp
@@ -0,0 +1,82 @@
+﻿#include "pch.h"
+
+#include "tuple.h"
+#include "util.h"
+
+void Tuples()
+{
+	PrintTitle("tuples");
+
+	const std::tuple<int, std::string, bool> t1(1, "Robert", true);
+	const auto t2 = std::make_tuple(2, "Anna", false);
+
+	std::cout << "t1 elements : ";
+	std::cout << std::get<0>(t1) << " ";
+	std::cout << std::get<1>(t1) << " ";
+	std::cout << std::get<2>(t1) << " ";
+	std::cout << std::endl;
+
+	std::cout << "t2 elements : ";
+	std::cout << std::get<0>(t2) << " ";
+	std::cout << std::get<1>(t2) << " ";
+	std::cout << std::get<2>(t2) << " ";
+	std::cout << std::endl;
+}
+
+void Tie()
+{
+	PrintTitle("tuple_tie");
+
+	const std::tuple<int, std::string, bool> t1(1, "Robert", true);
+	const auto t2 = std::make_tuple(2, "Anna", false);
+
+	auto i = 0;
+	std::string s = "";
+	auto b = false;
+
+	std::tie(i, s, b) = t1;
+	std::cout << "tie(s, i, b) = t1 : ";
+	std::cout << s << " ";
+	std::cout << i << " ";
+	std::cout << b << " ";
+	std::cout << std::endl;
+
+	std::tie(std::ignore, s, std::ignore) = t2;
+	std::cout << "tie(ignore, i, ignore) = t2 : ";
+	std::cout << s << " ";
+	std::cout << i << " ";
+	std::cout << b << " ";
+	std::cout << std::endl;
+}
+
+auto GetData(const int dataId)
+{
+	switch (dataId)
+	{
+	case 1:
+		return std::make_tuple(0, "Chloe", false);
+	case 2:
+		return std::make_tuple(1, "Bryan", true);
+	default:
+		return std::make_tuple(2, "Zoe", false);
+	}
+}
+
+void TupleValueReturn()
+{
+	PrintTitle("tuple_value_return");
+
+	auto people = GetData(1);
+	std::cout << "name = " << std::get<1>(people) << std::endl;
+	
+	std::string name = "";
+	std::tie(std::ignore, name, std::ignore) = people;
+	std::cout << "name = " << name << std::endl;
+}
+
+void TuplePractice()
+{
+	Tuples();
+	Tie();
+	TupleValueReturn();
+}
