62bb68003e5755401d52401a721721eadcb90e61
zrma <bulbitain@gmail.com>
2018-10-10 00:54:58 +0900

    c++ curry 연습 추가

---
+++ CppPractice/ModernCppFunctional/ModernCppFunctional/CurryPractice.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+
+#include "CurryPractice.h"
+#include "util.h"
+#include <boost/range/irange.hpp>
+
+
+void CurryPractice()
+{
+	PrintTitle("curry");
+
+	const auto width5 = Curry(AreaOfRect<int>, 5);
+	std::cout << "Curried with specific width = 5" << std::endl;
+
+	for( auto height : boost::irange(5))
+	{
+		std::cout << "Width5(" << height << ") = " << width5(height);
+		std::cout << std::endl;
+	}
+
+	const auto width5Height4 = Curry(VolumeOfRect<int>, 5, 4);
+	std::cout << "Curried with specific width = 5, height = 4" << std::endl;
+
+	for (auto depth : boost::irange(5))
+	{
+		std::cout << "Width5Height4(" << depth << ") = " << width5Height4(depth);
+		std::cout << std::endl;
+	}
+}
---
+++ CppPractice/ModernCppFunctional/ModernCppFunctional/CurryPractice.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+template<typename Func, typename... Args>
+auto Curry(Func func, Args... args)
+{
+	return [=](auto... others)
+	{
+		return func(args..., others...);
+	};
+}
+
+template<typename T>
+decltype(auto) AreaOfRect(const T width, const T height)
+{
+	return width * height;
+}
+
+template<typename T>
+decltype(auto) VolumeOfRect(const T width, const T height, const T depth)
+{
+	return width * height * depth;
+}
+
+void CurryPractice();
