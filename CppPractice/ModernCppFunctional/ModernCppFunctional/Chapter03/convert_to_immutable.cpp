7c11877590d099cdd87d03a547c5c0fc2ccf6186
zrma <bulbitain@gmail.com>
2018-10-14 16:04:26 +0900

    C++ 불변객체 활용 연습 추가

---
+++ CppPractice/ModernCppFunctional/ModernCppFunctional/Chapter03/convert_to_immutable.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+
+#include "convert_to_immutable.h"
+#include "../Common/util.h"
+
+void PrintImmutableEmployee(const ImmutableEmployee& me)
+{
+	std::cout << "Content of MutableEmployee instance" << std::endl;
+	std::cout << "Id : " << me.Id() << std::endl;
+	std::cout << "Name : " << me.FirstName();
+	std::cout << " " << me.LastName() << std::endl;
+	std::cout << "Salary : " << me.Salary() << std::endl;
+}
+
+void ConvertToImmutable()
+{
+	PrintTitle("convert_to_immutable");
+
+	const auto first = "Frankie";
+	const auto last = "Gamble";
+	const auto d = 1500.0;
+
+	const ImmutableEmployee me(0, first, last, d);
+	PrintImmutableEmployee(me);
+
+	const auto me2 = me.SetId(1);
+	const auto me3 = me.SetFirstName("Alexis");
+	const auto me4 = me.SetLastName("Andrew");
+	const auto me5 = me.SetSalary(2100.0);
+	PrintImmutableEmployee(me5);
+}
