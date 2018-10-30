a9a55460958b3e9568327dd3542884816e0f4f17
zrma <bulbitain@gmail.com>
2018-10-30 23:43:48 +0900

    C++ 꼬리재귀 최적화 연습

--- CppPractice/ModernCppFunctional/ModernCppFunctional/Chapter04/chapter04.cpp
+++ CppPractice/ModernCppFunctional/ModernCppFunctional/Chapter04/chapter04.cpp
@@ -3,9 +3,11 @@
 #include "chapter04.h"
 #include "factorial.h"
 #include "fibonacci.h"
+#include "tail_recursion.h"
 
 void Chapter04()
 {
 	FactorialPractice();
 	FibonacciPractice();
+	TailRecursionPractice();
 }
