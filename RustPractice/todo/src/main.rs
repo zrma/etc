43969c68a917312b9b9f031a771511c2fde50b6a
zrma <bulbitain@gmail.com>
2018-11-09 01:50:36 +0900

    traits + mod 연습 추가

--- RustPractice/todo/src/main.rs
+++ RustPractice/todo/src/main.rs
@@ -1,9 +1,11 @@
 #[macro_use]
 extern crate clap;
 
-
 use clap::App;
 
+mod task;
+use task::build_task;
+use task::Descript;
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
@@ -11,8 +13,13 @@ fn main() {
 
     if let Some(matches) = matches.subcommand_matches("add") {
         let input = matches.values_of("INPUT").unwrap();
-        let str = input.map(|s| &*s).collect::<Vec<&str>>().join(" ");
-        println!("add task {}", str);
+        let desc = input.map(|s| &*s).collect::<Vec<&str>>().join(" ");
+        println!("add task {}", desc);
+
+        let t = build_task(&*desc);
+        let text = t.description();
+
+        println!("{}", text)
     } else if let Some(matches) = matches.subcommand_matches("done") {
         let input = matches.value_of("INPUT").unwrap();
         println!("done task {}", input);
