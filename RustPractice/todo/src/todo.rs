29dd3b489d3b6d756cd3550b88c6f32c8e00475e
zrma <bulbitain@gmail.com>
2018-11-10 10:03:33 +0900

    Rust - Todo - Store 테스트 코드 추가 및 일부 기능 구현

---
+++ RustPractice/todo/src/todo.rs
@@ -0,0 +1,45 @@
+use task::Task;
+use task::build_task;
+
+pub trait Description {
+    fn description(&self) -> String;
+}
+
+pub struct Store {
+    tasks: Vec<Task>
+}
+
+
+impl Store {
+    pub fn add(&mut self, s: &str) {
+        let task = build_task(s);
+        self.tasks.push(task)
+    }
+
+    pub fn list(&self) -> &Vec<Task>{
+        return &self.tasks
+    }
+
+    pub fn done(&mut self, idx: usize) {
+        let task: &mut Task = &mut self.tasks[idx];
+        task.done = true;
+    }
+}
+
+impl Description for Vec<Task> {
+    fn description(&self) -> String {
+        let mut s = String::new();
+        for mut t in self {
+            s += &t.desc;
+        }
+
+        return s
+    }
+}
+
+
+pub fn todo() -> Store {
+    let v = vec![];
+
+    return Store { tasks: v};
+}
