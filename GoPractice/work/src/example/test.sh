5135ae4c4410ac12b76f26f5d06719fa4dfdd410
zrma <bulbitain@gmail.com>
2019-02-12 02:43:36 +0900

    코드 정리

--- GoPractice/work/src/example/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mkdir ./reports
-go-acc -o ./reports/coverage.out ./...
-go test ./... -json > ./reports/report.json
-go vet ./... 2> ./reports/govet-report.out
-gometalinter.v2 --vendor --deadline=300s --disable=dupl --linter='vet:go tool vet -printfuncs=Infof,Debugf,Warningf,Errorf:PATH:LINE:MESSAGE' ./... > ./reports/gometalinter-report.out
-gocov test ./... | gocov-html > ./reports/coverage.html
