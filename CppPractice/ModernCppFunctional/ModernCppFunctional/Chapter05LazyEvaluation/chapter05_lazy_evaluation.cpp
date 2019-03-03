7ccb0e9c60f0198f34e28b25103182d843423b98
zrma <bulbitain@gmail.com>
2019-03-03 20:09:32 +0900

    C++ 지연평가 연습

---
+++ CppPractice/ModernCppFunctional/ModernCppFunctional/Chapter05LazyEvaluation/chapter05_lazy_evaluation.cpp
@@ -0,0 +1,11 @@
+﻿#include "pch.h"
+
+#include "chapter05_lazy_evaluation.h"
+#include "strict.h"
+#include "non_strict.h"
+
+void chapter05_lazy_evaluation()
+{
+	strict();
+	non_strict();
+}
---
+++ CppPractice/ModernCppFunctional/ModernCppFunctional/Chapter05LazyEvaluation/chapter05_lazy_evaluation.h
@@ -0,0 +1,3 @@
+﻿#pragma once
+
+void chapter05_lazy_evaluation();
