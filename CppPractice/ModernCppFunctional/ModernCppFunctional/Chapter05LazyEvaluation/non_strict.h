1930da6c26c680befcd5d59f8bc7d74e2e12b369
zrma <bulbitain@gmail.com>
2019-03-03 20:09:32 +0900

    C++ 지연평가 연습

---
+++ CppPractice/ModernCppFunctional/ModernCppFunctional/Chapter05LazyEvaluation/non_strict.h
@@ -0,0 +1,4 @@
+﻿#pragma once
+
+auto outer_formula_non_strict(int x, int y, int z, std::function<int(int, int)> yz_func);
+void non_strict();
---
+++ CppPractice/ModernCppFunctional/ModernCppFunctional/Chapter05LazyEvaluation/strict.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+
+#include "strict.h"
+#include "../Common/util.h"
+
+auto outer_formula(const int x, const int yz)
+{
+	std::cout << "calculate " << x << " + ";
+	std::cout << "inner_formula(" << yz << ")";
+	std::cout << std::endl;
+
+	return x + yz;
+}
+
+int inner_formula(const int y, const int z)
+{
+	std::cout << "calculate " << y << " * ";
+	std::cout << z << std::endl;
+
+	return y * z;
+}
+
+void strict()
+{
+	print_title("strict");
+	const auto x = 4;
+	const auto y = 3;
+	const auto z = 2;
+
+	std::cout << "calculate " << x << " + ";
+	std::cout << "(" << y << " * " << z << ")";
+	std::cout << std::endl;
+	const auto result = outer_formula(x, inner_formula(y, z));
+
+	std::cout << x << " + ";
+	std::cout << "(" << y << " * " << z << ")";
+	std::cout << " = " << result << std::endl;
+}
