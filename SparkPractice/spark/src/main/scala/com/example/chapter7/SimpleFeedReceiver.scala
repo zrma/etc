98ff7a63b94431b9027f659d739d9de67f41ab39
zrma <bulbitain@gmail.com>
2019-12-26 10:40:25 +0900

    Modify some code to be enable standalone

--- SparkPractice/spark/src/main/scala/com/example/chapter7/SimpleFeedReceiver.scala
+++ SparkPractice/spark/src/main/scala/com/example/chapter7/SimpleFeedReceiver.scala
@@ -6,26 +6,23 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
+import scala.collection.mutable.ArrayBuffer
+
 case class SimpleReceiverConfig(hostname: String = "localhost",
                                 port: Int = 12345,
                                 interval: Int = 1000)
 
 object SimpleFeedReceiver extends Logging {
   def main(args: Array[String]) {
-
-    val parser = new scopt.OptionParser[SimpleReceiverConfig]("SimplePrinter") {
-      arg[String]("hostname") required () action { (x, c) =>
-        c.copy(hostname = x)
-      } text "The hostname to accept connections from remote hosts"
-
-      arg[Int]("port") required () action { (x, c) => c.copy(port = x)
-      } text "The port number to accept connections from remote hosts"
-
-      arg[Int]("interval") required () action { (x, c) => c.copy(interval = x)
-      } text "The interval to process data [msec]"
+    var params = new ArrayBuffer[String]()
+    params ++= args
+    if (args.length == 0) {
+      params += "localhost"
+      params += "12345"
+      params += "1000"
     }
 
-    parser.parse(args, SimpleReceiverConfig()) exists { config =>
+    Parser.receiver().parse(params.result(), SimpleReceiverConfig()) exists { config =>
       Logger.getRootLogger.setLevel(Level.WARN)
 
       val ss = SparkSession
