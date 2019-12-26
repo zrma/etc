ff685bffa3f67606cce1092b671f5d207402ad5b
zrma <bulbitain@gmail.com>
2019-12-27 01:51:57 +0900

    Add simple linear regression model example

--- SparkPractice/spark/src/main/scala/com/example/chapter8/SimpleLinearRegression.scala
+++ SparkPractice/spark/src/main/scala/com/example/chapter8/SimpleLinearRegression.scala
@@ -1,5 +1,8 @@
 package com.example.chapter8
 
+import org.apache.spark.mllib.feature.StandardScaler
+import org.apache.spark.mllib.regression.{LabeledPoint, LinearRegressionWithSGD}
+import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.Encoders
 import org.apache.spark.sql.functions._
@@ -57,8 +60,8 @@ object SimpleLinearRegression {
         t match {
           case x if x.contains("日") => 1d
           case x if x.contains("土") => 1d
-          case _                    => 0d
-      }
+          case _ => 0d
+        }
     )
 
     val replacedSalesAndWeatherDF = salesAndWeatherDF
@@ -66,5 +69,26 @@ object SimpleLinearRegression {
       .drop("day_of_week")
 
     replacedSalesAndWeatherDF.show(5)
+
+    val selectedDataDF = replacedSalesAndWeatherDF.select("sales", "avg_temp", "rainfall", "weekend")
+
+    import ss.implicits._
+    val labeledPoints = selectedDataDF.map { row => LabeledPoint(row.getDouble(0), Vectors.dense(row.getDouble(1), row.getDouble(2), row.getDouble(3))) }
+    val scaler = new StandardScaler(withMean = true, withStd = true).fit(labeledPoints.rdd.map(x => x.features))
+    val scaledLabeledPoints = labeledPoints.map { x => LabeledPoint(x.label, scaler.transform(x.features)) }
+
+    val iterCount = 20
+    val linearRegressionModel = LinearRegressionWithSGD.train(scaledLabeledPoints.rdd, iterCount)
+
+    val targetDataVector1 = Vectors.dense(15.0, 15.4, 1)
+    val targetDataVector2 = Vectors.dense(20.0, 0, 0)
+
+    val targetScaledDataVector1 = scaler.transform(targetDataVector1)
+    val targetScaledDataVector2 = scaler.transform(targetDataVector2)
+
+    println(linearRegressionModel.predict(targetScaledDataVector1))
+    println(linearRegressionModel.predict(targetScaledDataVector2))
+
+    println(linearRegressionModel.weights)
   }
 }
