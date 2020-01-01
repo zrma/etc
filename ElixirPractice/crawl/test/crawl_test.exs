2ccbc7da51e88bf9791184c62d310291ef4af34a
zrma <bulbitain@gmail.com>
2020-01-02 02:41:10 +0900

    Add some elixir test code examples

--- ElixirPractice/crawl/test/crawl_test.exs
+++ ElixirPractice/crawl/test/crawl_test.exs
@@ -5,4 +5,10 @@ defmodule CrawlTest do
   test "greets the world" do
     assert Crawl.hello() == :world
   end
+  test "greets with string args" do
+    assert Crawl.hello("greeting") == "greeting"
+  end
+  test "greets with list args" do
+    assert Crawl.hello(["a", "b", "c"]) == "a,b,c"
+  end
 end
