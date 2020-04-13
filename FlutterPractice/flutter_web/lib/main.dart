4d3716d6cddc7faa612be9dbc4757fa1797d6eb8
zrma <bulbitain@gmail.com>
2020-04-14 01:15:16 +0900

    FlutterPractice: add BottomNavigationBar example

--- FlutterPractice/flutter_web/lib/main.dart
+++ FlutterPractice/flutter_web/lib/main.dart
@@ -37,6 +37,30 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Call',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Message',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Setting',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,6 +79,11 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            Icon(
+              Icons.stars,
+              color: Colors.grey[500],
+            ),
+            _widgetOptions.elementAt(_selectedIndex),
           ],
         ),
       ),
@@ -62,6 +91,25 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.plus_one),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.phone),
+            title: Text('Call'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            title: Text('Message'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Setting'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
