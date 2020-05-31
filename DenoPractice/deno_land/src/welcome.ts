bfe799d8f9920013159134784f2b67596d2946b1
zrma <bulbitain@gmail.com>
2020-05-31 17:06:26 +0900

    DenoPractice: add JSON.parse / JSON.stringify usage

--- DenoPractice/deno_land/src/welcome.ts
+++ DenoPractice/deno_land/src/welcome.ts
@@ -5,3 +5,9 @@ const res = await fetch(url);
 
 const body = new Uint8Array(await res.arrayBuffer());
 await Deno.stdout.write(body);
+
+const str = "hello world"
+const jsonData = {str};
+
+console.log(JSON.stringify(jsonData));
+console.log(JSON.parse(`{"str":"${str}"}`));
