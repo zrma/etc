36f5f50746b588e04dfb81eb3ae6aae2d1f9560c
zrma <bulbitain@gmail.com>
2020-05-31 13:01:09 +0900

    DenoPractice: add http fetch example

--- DenoPractice/deno_land/src/welcome.ts
+++ DenoPractice/deno_land/src/welcome.ts
@@ -1,1 +1,7 @@
 console.log("Welcome to Deno 🦕");
+
+const url = Deno.args[0] || "https://github.com";
+const res = await fetch(url);
+
+const body = new Uint8Array(await res.arrayBuffer());
+await Deno.stdout.write(body);
