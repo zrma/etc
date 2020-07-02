972bf85906f409b1da919c3e99f4cfe1088eaf37
zrma <bulbitain@gmail.com>
2020-07-02 22:07:52 +0900

    CppPractice: change file encoding; UTF8 without BOM → with BOM

--- CppPractice/ModernCppFunctional/ModernCppFunctional/ModernCppFunctional.cpp
+++ CppPractice/ModernCppFunctional/ModernCppFunctional/ModernCppFunctional.cpp
@@ -1,4 +1,4 @@
-// ModernCppFunctional.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+﻿// ModernCppFunctional.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
 #include "pch.h"
