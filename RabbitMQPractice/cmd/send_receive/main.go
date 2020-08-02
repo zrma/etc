d321aab782e4104a99724976fd673769d3d00b11
zrma <bulbitain@gmail.com>
2020-08-02 15:46:38 +0900

    RabbitMQPractice: do refactoring; concatenate send and receive

---
+++ RabbitMQPractice/cmd/send_receive/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		send(cancel)
+		wg.Done()
+	}()
+	go func() {
+		receive(ctx)
+		wg.Done()
+	}()
+	wg.Wait()
+}
+
+func receive(ctx context.Context) {
+	conn, err := amqp.Dial("amqp://example:example@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	messages, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	for ctx.Err() == nil {
+		select {
+		case <-ctx.Done():
+			break
+		case msg := <-messages:
+			log.Printf("Received a message: %s", msg.Body)
+		}
+	}
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+}
+
+func send(cancel context.CancelFunc) {
+	conn, err := amqp.Dial("amqp://example:example@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	for i := 0; i < 10; i++ {
+		body := fmt.Sprintln("Hello World! #(", i, ")")
+		err = ch.Publish(
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		failOnError(err, "Failed to publish a message")
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	cancel()
+}
