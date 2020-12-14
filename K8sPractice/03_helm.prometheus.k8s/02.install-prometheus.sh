b0aeaefa38b529da9070f4978e7455943cd35bf5
zrma <bulbitain@gmail.com>
2020-12-15 08:44:42 +0900

    K8sPractice: add explicit version of prometheus helm chart

--- K8sPractice/03_helm.prometheus.k8s/02.install-prometheus.sh
+++ K8sPractice/03_helm.prometheus.k8s/02.install-prometheus.sh
@@ -2,4 +2,5 @@ helm install prometheus prometheus-community/prometheus \
   --namespace monitor \
   --set alertmanager.persistentVolume.enabled=false \
   --set server.persistentVolume.enabled=false \
-  --set pushgateway.persistentVolume.enabled=false
+  --set pushgateway.persistentVolume.enabled=false \
+  --version v11.16.2
