aa22d8660b2277381a6d9565fb827b6db927504b
zrma <bulbitain@gmail.com>
2020-12-14 11:01:57 +0900

    K8sPractice: remove dashboard step

--- K8sPractice/01_helm.dashboard.k8s/00.add-helm-repo.sh
+++
@@ -1,2 +0,0 @@
-helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
-helm repo update
--- K8sPractice/01_helm.dashboard.k8s/02.install-dashbard.sh
+++
@@ -1,4 +0,0 @@
-helm install \
-  kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard \
-  --namespace kubernetes-dashboard \
-  --set service.externalPort=8888
