e18390a8ca6dd3d0d6aabfc7cbbe2d20ca634255
zrma <bulbitain@gmail.com>
2021-04-25 19:55:53 +0900

    KotlinPractice: add compose for desktop example

---
+++ KotlinPractice/HelloCompose/build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.30"
+    id("org.jetbrains.compose") version "0.3.1"
+}
+
+group = "team504.zrma"
+version = "1.0"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+}
+
+dependencies {
+    testImplementation(kotlin("test-testng"))
+    implementation(compose.desktop.currentOs)
+}
+
+tasks.test {
+    useTestNG()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "HelloCompose"
+            packageVersion = "1.0.0"
+        }
+    }
+}
