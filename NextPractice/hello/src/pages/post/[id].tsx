80203fb68a86f1e90358222b3a61a76277955490
zrma <bulbitain@gmail.com>
2023-06-24 23:42:18 +0900

    NextPractice: add simple edgedb example

---
+++ NextPractice/hello/src/pages/post/[id].tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+
+import { client } from "@/pages/api/post";
+import e from "@/../dbschema/edgeql-js";
+
+export const getServerSideProps = async (
+  context?: GetServerSidePropsContext
+) => {
+  const post = await e
+    .select(e.BlogPost, (post) => ({
+      id: true,
+      title: true,
+      content: true,
+      filter_single: e.op(post.id, "=", e.uuid(context!.params!.id as string)),
+    }))
+    .run(client);
+  return { props: { post: post! } };
+};
+
+export type GetPost = InferGetServerSidePropsType<typeof getServerSideProps>;
+
+const Post: React.FC<GetPost> = (props) => {
+  return (
+    <div
+      style={{
+        margin: "auto",
+        width: "100%",
+        maxWidth: "600px",
+      }}
+    >
+      <h1 style={{ padding: "50px 0px" }}>{props.post.title}</h1>
+      <p style={{ color: "#666" }}>{props.post.content}</p>
+    </div>
+  );
+};
+
+export default Post;
