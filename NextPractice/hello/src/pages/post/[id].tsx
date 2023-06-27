2a11032da7a543b58ee183450fa54addedd226a1
zrma <bulbitain@gmail.com>
2023-06-28 00:24:25 +0900

    NextPractice: modify design

--- NextPractice/hello/src/pages/post/[id].tsx
+++ NextPractice/hello/src/pages/post/[id].tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import "@/app/globals.css";
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
-
 import { client } from "@/pages/api/post";
 import e from "@/../dbschema/edgeql-js";
 
@@ -32,16 +31,18 @@ export type GetPost = InferGetServerSidePropsType<typeof getServerSideProps>;
 
 const Post: React.FC<GetPost> = (props) => {
   return (
-    <div
-      style={{
-        margin: "auto",
-        width: "100%",
-        maxWidth: "600px",
-      }}
-    >
-      <h1 style={{ padding: "50px 0px" }}>{props.post.title}</h1>
-      <p style={{ color: "#666" }}>{props.post.content}</p>
-      <p style={{ color: "#999" }}>{props.post.created_at}</p>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200">
+      <main className="w-full max-w-2xl px-4 py-8 mx-auto bg-gray-300 rounded-lg shadow-md h-[720px] overflow-auto">
+        <h1 className="text-3xl font-semibold text-center text-gray-700">
+          {props.post.title}
+        </h1>
+        <p className="mt-4 text-xl font-semibold text-gray-700">
+          {props.post.content}
+        </p>
+        <p className="mt-4 text-l text-right font-semibold text-gray-500">
+          {new Date(props.post.created_at).toLocaleString()}
+        </p>
+      </main>
     </div>
   );
 };
