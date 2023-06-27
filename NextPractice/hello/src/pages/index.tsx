fe1cb54032f4445aafd615f978f8d0cec2e17016
zrma <bulbitain@gmail.com>
2023-06-28 00:24:25 +0900

    NextPractice: modify design

--- NextPractice/hello/src/pages/index.tsx
+++ NextPractice/hello/src/pages/index.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Head from "next/head";
 import "@/app/globals.css";
 import { GetServerSideProps } from "next";
@@ -10,7 +9,7 @@ export default function Home({ posts }: { posts: Posts }) {
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <Head>
           <title>Loading...</title>
-          <meta name="description" content="My awesome blog" />
+          <meta name="description" content="My awesome memo site" />
           <link rel="icon" href="/src/app/favicon.ico" />
         </Head>
       </div>
@@ -18,25 +17,44 @@ export default function Home({ posts }: { posts: Posts }) {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200">
       <Head>
-        <title>My Blog</title>
-        <meta name="description" content="My awesome blog" />
+        <title>Memo</title>
+        <meta name="description" content="My awesome memo site" />
         <link rel="icon" href="/src/app/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">Blog</h1>
-        <div style={{ height: "50px" }}></div>
-        {posts.map((post) => {
-          return (
-            <a href={`/post/${post.id}`} key={post.id}>
-              <div className={`mb-3 text-2xl font-semibold`}>
-                <p>{post.title}</p>
-              </div>
-            </a>
-          );
-        })}
+      <main className="w-full max-w-2xl px-4 py-8 mx-auto bg-gray-300 rounded-lg shadow-md h-[720px] overflow-auto">
+        <h1 className="text-3xl font-semibold text-center text-gray-700">
+          Memo
+        </h1>
+        <div className="mt-4">
+          <textarea
+            id="new-msg-input"
+            name="new-msg-input"
+            placeholder="Write your new post..."
+            className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
+            rows={4}
+          />
+          <button
+            type="submit"
+            className="w-full px-4 py-2 mt-2 font-semibold text-white bg-blue-400 rounded hover:bg-blue-300"
+          >
+            Post
+          </button>
+        </div>
+
+        <div className="mt-6">
+          {posts.map((post) => {
+            return (
+              <a href={`/post/${post.id}`} key={post.id} className="block mt-4">
+                <div className="text-xl font-semibold text-gray-700">
+                  {post.title}
+                </div>
+              </a>
+            );
+          })}
+        </div>
       </main>
     </div>
   );
