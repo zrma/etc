795605b7d7ab0fa6752e35a6948c32d63492ec65
zrma <bulbitain@gmail.com>
2023-06-29 00:03:06 +0900

    NextPractice: write new post example

--- NextPractice/hello/src/pages/api/post.ts
+++ NextPractice/hello/src/pages/api/post.ts
@@ -16,12 +16,36 @@ const selectPosts = e.select(e.BlogPost, (post) => ({
   },
 }));
 
+const insertPost = (title: string, content: string) =>
+  e.insert(e.BlogPost, {
+    title: title,
+    content: content,
+  });
+
 export type Posts = $infer<typeof selectPosts>;
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const posts = await selectPosts.run(client);
-  res.status(200).json(posts);
+  const actions = {
+    POST: async () => {
+      const { title, content } = req.body;
+      await insertPost(title, content).run(client);
+      res.status(200).json({ message: "Post created" });
+    },
+    GET: async () => {
+      const posts = await selectPosts.run(client);
+      res.status(200).json(posts);
+    },
+  };
+
+  const method = req.method as "POST" | "GET";
+  const action = actions[method];
+
+  if (action) {
+    await action();
+  } else {
+    res.status(405).end(); // Method Not Allowed
+  }
 }
